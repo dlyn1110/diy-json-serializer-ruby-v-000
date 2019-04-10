5844e25880956ee9bd9330280c5e9c88ab1aa64f
Dani Lyn <dlyn1110@gmail.com>
2019-04-10 20:07:16 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,11 @@ class PostsController < ApplicationController
    render json: PostSerializer.serialize(post)
   end
 
+  def post_data
+    post = Post.find(params[:id])
+    render json: PostSerializer.serialize(post)
+  end
+
   def update
     @post.update(post_params)
     redirect_to post_path(@post)
